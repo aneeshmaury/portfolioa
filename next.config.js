74577fa24a2cec1b4b66782389385136d716a718
shayaliza <magicalmind01@gmail.comgit statuss>
2023-12-20 11:41:30 +0530

    ssa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const isProd = (process.env.NODE_ENV = "production");
+
 module.exports = nextConfig;
 module.exports = {
+  basePath: isProd ? "/portfolio" : "",
   output: "export",
   distDir: "dist",
   images: {
